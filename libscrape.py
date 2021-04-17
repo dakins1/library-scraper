7c05ea9bbbfb57be0706ac12c8e8a0a061adbfbf
Dillon Akins <dakins1@trinity.edu>
2021-04-17 11:18:51 -0500

    multithreading added, performance significantly increased

--- libscrape.py
+++ libscrape.py
@@ -1,5 +1,6 @@
 import json
 import time
+import concurrent.futures 
 from collections import deque
 import requests
 import pandas as pd
@@ -8,37 +9,32 @@ import config
 #from urllib.parse import urlsplit
 #import re
 
-libdata = pd.read_excel("file:///mnt/c/Users/Dillon/comp/UnitedStatesPublicLibrariesContactInfo-ChildrensProgramming.xlsx")
-libdata['searchVals'] = libdata['Name'] + " " + libdata['City'] + " " + libdata['State']
-searchVals = libdata['searchVals'].tolist()
-searchVals = searchVals[:10]
-searchVals.append("fauiwehfgnp;iAOUWENvgjpo;iAJWRhbgahwieuhyfaliUWehfouaw")
-
 SEARCH_URL = "https://api.bing.microsoft.com/v7.0/search"
 headers = {"Ocp-Apim-Subscription-Key": config.SUBSCRIPTION_KEY}
 
-web_list = deque([])
-failures = deque([])
-
-t0 = time.perf_counter()
-for sv in searchVals:
+def get_websites(sv):
     params = {"q": sv, "textDecorations": True, "textFormat": "HTML"}
     response = requests.get(SEARCH_URL, headers=headers, params=params)
     response.raise_for_status()
     search_results = response.json()
     jval = json.JSONDecoder().decode(json.dumps(search_results))
     try:
-        web_list.append(jval['webPages']['value'][0]['url'])
+        res = jval['webPages']['value'][0]['url']
     except (KeyError, ValueError):
-        failures.append(sv)
-t1=time.perf_counter()
-size = len(web_list)
-print("size")
-print(size)
-print("time")
-print(t1-t0)
-print("failures")
-print(failures)
+        res = "FAILURE " + sv
+    return res
 
-
-
+if __name__ == "__main__":
+    libdata = pd.read_excel(config.FILE_PATH)
+    libdata['searchVals'] = libdata['Name'] + " " + libdata['City'] + " " + libdata['State']
+    searchVals = libdata['searchVals'].tolist()
+    searchVals = searchVals[:100]
+    searchVals.append("fauiwehfgnp;iAOUWENvgjpo;iAJWRhbgahwieuhyfaliUWehfouaw")
+    t0 = time.perf_counter()
+    #list_of_results = []
+    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+        list_of_results = list(executor.map(get_websites, searchVals))
+    t1 = time.perf_counter()
+    print(list(list_of_results))
+    print("time: ")
+    print(t1-t0)
