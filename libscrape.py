1000f9689099be706621f2ddc0189845f79fbf98
Dillon Akins <dakins1@trinity.edu>
2021-04-17 12:06:09 -0500

    added library data with search results

--- libscrape.py
+++ libscrape.py
@@ -12,8 +12,8 @@ import config
 SEARCH_URL = "https://api.bing.microsoft.com/v7.0/search"
 headers = {"Ocp-Apim-Subscription-Key": config.SUBSCRIPTION_KEY}
 
-def get_websites(sv):
-    params = {"q": sv, "textDecorations": True, "textFormat": "HTML"}
+def get_websites(sd):
+    params = {"q": sd[0], "textDecorations": True, "textFormat": "HTML"}
     response = requests.get(SEARCH_URL, headers=headers, params=params)
     response.raise_for_status()
     search_results = response.json()
@@ -21,19 +21,19 @@ def get_websites(sv):
     try:
         res = jval['webPages']['value'][0]['url']
     except (KeyError, ValueError):
-        res = "FAILURE " + sv
-    return res
+        res = "FAILURE " + sd[0]
+    return res, sd[1], sd[2], sd[3]
 
 if __name__ == "__main__":
     libdata = pd.read_excel(config.FILE_PATH)
-    libdata['searchVals'] = libdata['Name'] + " " + libdata['City'] + " " + libdata['State']
-    searchVals = libdata['searchVals'].tolist()
-    searchVals = searchVals[:100]
-    searchVals.append("fauiwehfgnp;iAOUWENvgjpo;iAJWRhbgahwieuhyfaliUWehfouaw")
+    libdata['searchVal'] = libdata['Name'] + " " + libdata['City'] + " " + libdata['State']
+    searchData = list(libdata[['searchVal', 'Name', 'City', 'State']].itertuples(index=False, name=None))
+    searchData = searchData[:100]
+    searchData.append(("fauiwehfgnp;iAOUWENvgjpo;iAJWRhbgahwieuhyfaliUWehfouaw", 'no', 'city', 'here'))
     t0 = time.perf_counter()
     #list_of_results = []
     with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-        list_of_results = list(executor.map(get_websites, searchVals))
+        list_of_results = list(executor.map(get_websites, searchData))
     t1 = time.perf_counter()
     print(list(list_of_results))
     print("time: ")
